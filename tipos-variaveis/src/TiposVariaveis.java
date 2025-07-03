2a19ed860d9145affc312f41703f91b7553a20c3
Henrique <henriquejesusmendesmachadoo@gmail.com>
2025-07-03 15:56:34 -0300

    Estudos com Tipos e Váriaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,2 +1,13 @@
 public class TiposVariaveis {
+    public static void main(String[] args ){
+        short salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = 1;
+        short numeroCurto2 = (short) numeroNormal;
+        final int MINHAVARIAVEL = 10;
+        System.out.print(MINHAVARIAVEL);
+        // Para colocar uma variavel de valor imutavel (CONSTANTE) usamos o "final" seguido do nome da variável em maiúscula
+        // ex: final int MINHA_VARIAVEL = 10;
+    }
 }
